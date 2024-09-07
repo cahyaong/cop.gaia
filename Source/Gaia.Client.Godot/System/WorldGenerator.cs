9daedd3093769834a943834305e603cb7657297b
Cahya Ong <cahya.ong@gmail.com>
2024-09-07 12:57:11 -0700

    Added new Godot project.

---
+++ Source/Gaia.Client.Godot/System/WorldGenerator.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WorldGenerator.cs" company="nGratis">
+//  The MIT License -- Copyright (c) Cahya Ong
+//  See the LICENSE file in the project root for more information.
+// </copyright>
+// <author>Cahya Ong -- cahya.ong@gmail.com</author>
+// <creation_timestamp>Saturday, September 7, 2024 7:48:27 PM UTC</creation_timestamp>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Godot;
+
+public partial class WorldGenerator : Node2D
+{
+}
